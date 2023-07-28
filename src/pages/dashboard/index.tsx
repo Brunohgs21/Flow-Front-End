028539577ae63c74de6e09b4324ddea819cc578c
Brunohgs21 <brunohgs21@outlook.com>
2023-07-28 12:23:49 -0300

    feat: login and dashboard base

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { useAuth } from "../../hooks/useAuth";
+
+export interface Contact {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  createdAt: string;
+}
+
 export const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const [contacts, setContacts] = useState<Contact[]>([]);
+  const { loading } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get<Contact[]>("/contacts");
+      setContacts(response.data);
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,28 @@
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { LoginData, schema } from "./validator";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login = () => {
   const { register, handleSubmit } = useForm<LoginData>({
     resolver: zodResolver(schema),
   });
+  const submit: SubmitHandler<LoginData> = async (data) => {
+    signIn(data);
+  };
+
+  const { signIn } = useAuth();
+
   return (
     <main>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit()}>
+      <form onSubmit={handleSubmit(submit)}>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" {...register("email")} />
         <label htmlFor="password">Senha</label>
         <input type="password" id="password" {...register("password")} />
+        <button type="submit">Entrar</button>
       </form>
     </main>
   );
 };
-// DEVELOP:
